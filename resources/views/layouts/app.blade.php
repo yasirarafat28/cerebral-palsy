c5eaee98945ff71194e5400bac1b277602d3f3eb
Md. Yasir Aarafat <39982576+yasirarafat28@users.noreply.github.com>
2021-01-16 19:49:23 +0600

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -104,13 +104,13 @@
         </div>
 
         <!-- Slide 3 -->
-        <div class="carousel-item" style="background-image: url(/front/img/slide-3.jpg)">
+        {{-- <div class="carousel-item" style="background-image: url(/front/img/slide-3.jpg)">
           <div class="container">
             <h2>Sequi ea ut et est quaerat</h2>
             <p>Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel.</p>
             <a href="#about" class="btn-get-started scrollto">Read More</a>
           </div>
-        </div>
+        </div> --}}
         <div class="carousel-item" style="background-image: url(/front/img/slide-4.jpg)">
           <div class="container">
             <h2>Sequi ea ut et est quaerat</h2>
