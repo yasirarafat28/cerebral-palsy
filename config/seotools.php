53062e584d66624583e53faaa92aa672c51c61cc
Md. Yasir Aarafat <39982576+yasirarafat28@users.noreply.github.com>
2021-01-17 21:33:13 +0600

    Update seotools.php

--- config/seotools.php
+++ config/seotools.php
@@ -38,8 +38,8 @@ return [
          * The default configurations to be used by the opengraph generator.
          */
         'defaults' => [
-            'title'       => 'Over 9000 Thousand!', // set false to total remove
-            'description' => 'For those who helped create the Genki Dama', // set false to total remove
+            'title'       => env('APP_NAME'), // set false to total remove
+            'description' => '', // set false to total remove
             'url'         => false, // Set null for using Url::current(), set false to total remove
             'type'        => false,
             'site_name'   => false,
@@ -60,8 +60,8 @@ return [
          * The default configurations to be used by the json-ld generator.
          */
         'defaults' => [
-            'title'       => 'Over 9000 Thousand!', // set false to total remove
-            'description' => 'For those who helped create the Genki Dama', // set false to total remove
+            'title'       => env('APP_NAME'), // set false to total remove
+            'description' => '', // set false to total remove
             'url'         => false, // Set null for using Url::current(), set false to total remove
             'type'        => 'WebPage',
             'images'      => [],
